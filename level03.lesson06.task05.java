ba58ba09b34e61bd8bfeec1f55778d8f4ba5c0d5
DenisTmenov <denis.tmenov@gmail.com>
2017-01-10 16:26:27 +0300

    Create

---
+++ level03.lesson06.task05.java
@@ -0,0 +1,17 @@
+package com.javarush.test.level03.lesson06.task05;
+
+/* Изучаем японский
+Выведи на экран 日本語
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        //напишите тут ваш код
+
+       String jap1 = "日本語";
+
+        System.out.print(jap1);
+    }
+}
---
+++ level03.lesson08.task01.java
@@ -0,0 +1,28 @@
+package com.javarush.test.level03.lesson08.task01;
+
+/* Как захватить мир
+Ввести с клавиатуры число и имя, вывести на экран строку:
+«имя» захватит мир через «число» лет. Му-ха-ха!
+Пример: Вася захватит мир через 8 лет. Му-ха-ха!
+
+Последовательность вводимых данных имеет большое значение.
+*/
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        //напишите тут ваш код
+
+        Scanner scanner = new Scanner(System.in);
+
+        int age = scanner.nextInt();
+        String name = scanner.nextLine();
+
+        System.out.println(name + " захватит мир через " + age + " лет. Му-ха-ха!");
+
+    }
+}
